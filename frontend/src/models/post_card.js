c5321f51f24f464ae1d4852ca69491ea15937350
petermking18 <petermking18@gmail.com>
2020-11-11 11:53:11 -0600

    added post_card model

---
+++ frontend/src/models/post_card.js
@@ -0,0 +1,11 @@
+export class post_card{
+    constructor(id,username,date,location,imgurl,text,numComments){
+        this.id = id;
+        this.username = username;
+        this.date = date;
+        this.location = location;
+        this.imgurl = imgurl;
+        this.text = text;
+        this.numComments = numComments;
+    }
+}
