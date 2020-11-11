9a71e185425a0ea907727d9007a9d0e2de191d04
David Hoffman <dghoffman@smu.edu>
2020-11-11 16:31:55 -0600

    updating routes.js

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,14 +2,15 @@ import Login from './Components/login';
 import RegisterPage from './Components/register';
 import MainPage  from './Components/mainPage';
 import Search from './Components/search';
-import ProfilePage from './Components/profilePage';
+import PersonalProfilePage from './Components/personalProfile';
+import OtherProfilePage from './Components/otherProfile';
 import DrugForm from "./Components/newDrugPage";
 
 export const ROUTES = [
     { path: '/search/:id', component: Search },
     { path: '/dashboard/:id', component: MainPage },
-    { path: '/profile/:id', component: ProfilePage},
+    { path: '/personalprofile/:id', component: PersonalProfilePage},
+    { path: '/otherprofile/:id', component: OtherProfilePage},
     { path: '/register', component: RegisterPage },
-    { path: '/update/:id/:drugId', component: DrugForm },
     { path: '/', component: Login }
 ]
