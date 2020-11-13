affaab4ce4a92c94f9449e24391183f386e20fa2
Nathan Gage <ngage@smu.edu>
2020-11-13 04:03:25 -0600

    Comment out old express code

--- backend/express/server.js
+++ backend/express/server.js
@@ -47,55 +47,55 @@ app.get('/', (req, res) => {
 });
 
 
-// POST /reset
-app.post('/reset', (req, res) => {
-  connection.query('drop table if exists test_table', function (err, rows, fields) {
-    if (err)
-      logger.error("Can't drop table");
-  });
-  connection.query('CREATE TABLE `db`.`test_table` (`id` INT NOT NULL AUTO_INCREMENT, `value` VARCHAR(45), PRIMARY KEY (`id`), UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);', function (err, rows, fields) {
-    if (err)
-      logger.error("Problem creating the table test_table");
-  });
-  res.status(200).send('created the table');
-});
+// // POST /reset
+// app.post('/reset', (req, res) => {
+//   connection.query('drop table if exists test_table', function (err, rows, fields) {
+//     if (err)
+//       logger.error("Can't drop table");
+//   });
+//   connection.query('CREATE TABLE `db`.`test_table` (`id` INT NOT NULL AUTO_INCREMENT, `value` VARCHAR(45), PRIMARY KEY (`id`), UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);', function (err, rows, fields) {
+//     if (err)
+//       logger.error("Problem creating the table test_table");
+//   });
+//   res.status(200).send('created the table');
+// });
 
-// POST /multplynumber
-app.post('/multplynumber', (req, res) => {
-  console.log(req.body.product);
+// // POST /multplynumber
+// app.post('/multplynumber', (req, res) => {
+//   console.log(req.body.product);
 
-  connection.query('INSERT INTO `db`.`test_table` (`value`) VALUES(\'' + req.body.product + '\')', function (err, rows, fields) {
-    if (err){
-      logger.error("Problem inserting into test table");
-    }
-    else {
-      res.status(200).send(`added ${req.body.product} to the table!`);
-    }
-  });
-});
+//   connection.query('INSERT INTO `db`.`test_table` (`value`) VALUES(\'' + req.body.product + '\')', function (err, rows, fields) {
+//     if (err){
+//       logger.error("Problem inserting into test table");
+//     }
+//     else {
+//       res.status(200).send(`added ${req.body.product} to the table!`);
+//     }
+//   });
+// });
 
-// GET /checkdb
-app.get('/values', (req, res) => {
-  connection.query('SELECT value FROM `db`.`test_table`', function (err, rows, fields) {
-    if (err) {
-      logger.error("Error while executing Query: \n", err);
-      res.status(400).json({
-        "data": [],
-        "error": "MySQL error"
-      })
-    }
-    else{
-      res.status(200).json({
-        "data": rows
-      });
-    }
-  });
-});
+// // GET /checkdb
+// app.get('/values', (req, res) => {
+//   connection.query('SELECT value FROM `db`.`test_table`', function (err, rows, fields) {
+//     if (err) {
+//       logger.error("Error while executing Query: \n", err);
+//       res.status(400).json({
+//         "data": [],
+//         "error": "MySQL error"
+//       })
+//     }
+//     else{
+//       res.status(200).json({
+//         "data": rows
+//       });
+//     }
+//   });
+// });
 
-// connecting the express object to listen on a particular port as defined in the config object.
-app.listen(config.port, config.host, (e) => {
-  if (e) {
-    throw new Error('Internal Server Error');
-  }
-  logger.info(`${config.name} running on ${config.host}:${config.port}`);
-});
+// // connecting the express object to listen on a particular port as defined in the config object.
+// app.listen(config.port, config.host, (e) => {
+//   if (e) {
+//     throw new Error('Internal Server Error');
+//   }
+//   logger.info(`${config.name} running on ${config.host}:${config.port}`);
+// });
