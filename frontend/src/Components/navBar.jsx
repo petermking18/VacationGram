ccb23fddcf0987004adcff7a62280d8b5e6b6770
petermking18 <petermking18@gmail.com>
2020-11-21 18:14:46 -0600

    new nav bar, some bugs

--- frontend/src/Components/navBar.jsx
+++ frontend/src/Components/navBar.jsx
@@ -1,75 +1,63 @@
 import React from 'react';
 import { Redirect, Link } from 'react-router-dom';
-import './navBar.css';
+import './NavBar.css';
 import logo from './mainPageLogo.png';
 
-class NavBar extends React.Component{
-	constructor(props)
-	{
-		super(props);
-		this.state = {
-			id: '',
-			profile: false,
-			logout: false,
-			search: false,
-			home: false,
-			newPost: false
-		};
-	}
+export default class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: '',
+            profile: false,
+            logout: false,
+            search: false,
+            home: false,
+        };
+    }
 
-	logoutUser = e => {
-        this.setState({logout: true});
-	}
+    logoutUser = e => {
+        this.setState({ logout: true });
+    }
 
-	goToProfile = e => {
-        this.setState({profile: true});
-	}
-	goToHome = e => {
-        this.setState({home: true});
-	}
+    goToProfile = e => {
+        this.setState({ profile: true });
+    }
+    goToHome = e => {
+        this.setState({ home: true });
+    }
 
-	goToSearch = e => {
-		this.setState({search: true});
-	}
+    goToSearch = e => {
+        this.setState({ search: true });
+    }
 
-	goToNewPost = e => {
-		this.setState({newPost: true});
-	}
-	
-
-	render(){
-		return <>
-		<div>
-			<nav>
-				<ul>
-					<li className = "Title">
-						<Link to={'/home/'} id="title">
-							<img src={logo} alt="Avatar" className="avatarNav"></img>
-							TravelGram
-						</Link>
-					</li>
-					<li>
-						<a href = "" onClick = {this.logoutUser}>Logout</a>
-						{this.state.logout  && <Redirect to="/login" /> }
-					</li>
-					<li>
-						<a href="" onClick={this.goToSearch}>Search</a>
-						{this.state.search  && <Redirect to={'/search/:id'}/> }
-					</li>
-					<li>
+    render() {
+        return <>
+            <header class="header">
+                <Link to={'/home/'} id="title" class="logo">
+                    <img src={logo} alt="Avatar" className="avatarNav"></img>
+					TravelGram
+                </Link>
+                <input class="menu-btn" type="checkbox" id="menu-btn" />
+                <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+                <ul class="menu">
+                    <li>
+                        <a href="" onClick={this.goToHome}>Feed</a>
+                        {this.state.home &&<Redirect to={'/home/'}></Redirect>}
+                    </li>
+                    <li>
 						<a href="" onClick = {this.goToProfile}>Profile</a>
 						{this.state.profile  &&<Redirect to={'/profile/:id'}></Redirect>}
 					</li>
-					<li>
-						{/* <a onClick = {this.goToDash} href="">Feed</a>
-						{this.state.dash &&<Redirect to={'/home/' + this.props.id}></Redirect>} */}
-						<a onClick = {this.goToHome} href="">Feed</a>
-						{this.state.home &&<Redirect to={'/home/'}></Redirect>}
+                    <li>
+						<a href="" onClick={this.goToSearch}>Search</a>
+						{this.state.search  && <Redirect to={'/search/:id'}/> }
 					</li>
-				</ul>
-			</nav>
-		</div>
-		</>
-	}
+                    <li>
+						<a href = "" onClick = {this.logoutUser}>Logout</a>
+						{this.state.logout  && <Redirect to="/login" /> }
+					</li>
+                </ul>
+            </header>
+        </>
+    }
 }
-export default NavBar;
