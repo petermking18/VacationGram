8a8dfad78eb611416912c1d6f03f202d2c9741e2
Nathan Gage <ngage@smu.edu>
2020-11-28 13:58:48 -0600

    Update schema

--- backend/vacationgram_schema.sql
+++ backend/vacationgram_schema.sql
@@ -14,7 +14,8 @@ SET
   -- -----------------------------------------------------
   -- Schema db
   -- -----------------------------------------------------
-  -- -----------------------------------------------------
+  DROP SCHEMA IF EXISTS `db`;
+-- -----------------------------------------------------
   -- Schema db
   -- -----------------------------------------------------
   CREATE SCHEMA IF NOT EXISTS `db` DEFAULT CHARACTER SET utf8;
@@ -32,17 +33,15 @@ CREATE TABLE IF NOT EXISTS `db`.`user` (
     `email` VARCHAR(45) NOT NULL,
     `password` VARCHAR(45) NOT NULL,
     `date_joined` DATETIME NOT NULL,
-    `username` VARCHAR(20) NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-    UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,
     UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE
   ) ENGINE = InnoDB AUTO_INCREMENT = 32 DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
-  -- Table `db`.`lkp_trip_reaction`
+  -- Table `db`.`lkp_reaction`
   -- -----------------------------------------------------
-  DROP TABLE IF EXISTS `db`.`lkp_trip_reaction`;
-CREATE TABLE IF NOT EXISTS `db`.`lkp_trip_reaction` (
+  DROP TABLE IF EXISTS `db`.`lkp_reaction`;
+CREATE TABLE IF NOT EXISTS `db`.`lkp_reaction` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(45) NOT NULL,
     PRIMARY KEY (`id`),
@@ -66,12 +65,13 @@ CREATE TABLE IF NOT EXISTS `db`.`trip` (
     `is_public` BIT(1) NOT NULL DEFAULT b '1',
     `date_created` DATETIME NOT NULL,
     `date_last_updated` DATETIME NULL DEFAULT NULL,
+    `image_url` VARCHAR(100) NULL,
     PRIMARY KEY (`id`),
     UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
     INDEX `user_id_idx` (`user_id` ASC) VISIBLE,
     INDEX `sentiment_id_idx` (`reaction_id` ASC) VISIBLE,
     CONSTRAINT `fk_user_trip_id` FOREIGN KEY (`user_id`) REFERENCES `db`.`user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT `reaction_id` FOREIGN KEY (`reaction_id`) REFERENCES `db`.`lkp_trip_reaction` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT `reaction_id` FOREIGN KEY (`reaction_id`) REFERENCES `db`.`lkp_reaction` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
   ) ENGINE = InnoDB AUTO_INCREMENT = 13 DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
   -- Table `db`.`comment`
@@ -82,7 +82,6 @@ CREATE TABLE IF NOT EXISTS `db`.`comment` (
     `trip_id` INT NOT NULL,
     `user_id` INT NOT NULL,
     `parent_comment_id` INT NULL DEFAULT NULL COMMENT 'parent comment id, if null it is root comment\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\nused for replies',
-    `is_question` BIT(1) NOT NULL DEFAULT b '0',
     `date_created` DATETIME NOT NULL,
     `body` VARCHAR(240) NOT NULL,
     PRIMARY KEY (`id`),
@@ -133,7 +132,6 @@ CREATE TABLE IF NOT EXISTS `db`.`user_saved_trip` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `trip_id` INT NOT NULL,
     `saved_by_user_id` INT NOT NULL,
-    `save_ranking` INT NOT NULL DEFAULT '0',
     PRIMARY KEY (`id`),
     UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
     INDEX `trip_id_idx` (`trip_id` ASC) VISIBLE,
@@ -147,3 +145,277 @@ SET
   FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS;
 SET
   UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS;
+-- -----------------------------------------------------
+  -- Data for table `db`.`user`
+  -- -----------------------------------------------------
+  START TRANSACTION;
+USE `db`;
+INSERT INTO
+  `db`.`user` (
+    `id`,
+    `name`,
+    `birthdate`,
+    `location`,
+    `bio`,
+    `email`,
+    `password`,
+    `date_joined`
+  )
+VALUES
+  (
+    1,
+    'Nathan Gage',
+    '2002-04-04T16:25:31.000Z',
+    'Dallas',
+    'Loading...',
+    'nathan@gage.com',
+    'password',
+    '2020-11-28T19:23:31.000Z'
+  );
+INSERT INTO
+  `db`.`user` (
+    `id`,
+    `name`,
+    `birthdate`,
+    `location`,
+    `bio`,
+    `email`,
+    `password`,
+    `date_joined`
+  )
+VALUES
+  (
+    2,
+    'Smith Smith',
+    '1990-05-03T12:00:00.000Z',
+    'Everywhere',
+    'I\'m just a dood',
+    'smith@smith.com',
+    'password',
+    '2020-11-28T19:23:31.000Z'
+  );
+COMMIT;
+-- -----------------------------------------------------
+  -- Data for table `db`.`lkp_reaction`
+  -- -----------------------------------------------------
+  START TRANSACTION;
+USE `db`;
+INSERT INTO
+  `db`.`lkp_reaction` (`id`, `name`)
+VALUES
+  (1, 'Boring');
+INSERT INTO
+  `db`.`lkp_reaction` (`id`, `name`)
+VALUES
+  (2, 'Exciting');
+INSERT INTO
+  `db`.`lkp_reaction` (`id`, `name`)
+VALUES
+  (3, 'Tedious');
+INSERT INTO
+  `db`.`lkp_reaction` (`id`, `name`)
+VALUES
+  (4, 'Sad');
+INSERT INTO
+  `db`.`lkp_reaction` (`id`, `name`)
+VALUES
+  (5, 'Average');
+INSERT INTO
+  `db`.`lkp_reaction` (`id`, `name`)
+VALUES
+  (6, 'Relaxing');
+COMMIT;
+-- -----------------------------------------------------
+  -- Data for table `db`.`trip`
+  -- -----------------------------------------------------
+  START TRANSACTION;
+USE `db`;
+INSERT INTO
+  `db`.`trip` (
+    `id`,
+    `user_id`,
+    `title`,
+    `body`,
+    `price`,
+    `origin`,
+    `destination`,
+    `rating`,
+    `reaction_id`,
+    `is_public`,
+    `date_created`,
+    `date_last_updated`,
+    `image_url`
+  )
+VALUES
+  (
+    1,
+    1,
+    'Nate\'s First Trip',
+    'This was a decent trip',
+    4,
+    'The API',
+    'Amazon RDS',
+    3,
+    5,
+    DEFAULT,
+    '2020-11-28T19:23:31.000Z',
+    '2020-11-28T19:23:31.000Z',
+    'https://i.imgur.com/ozjwWqC.jpeg'
+  );
+INSERT INTO
+  `db`.`trip` (
+    `id`,
+    `user_id`,
+    `title`,
+    `body`,
+    `price`,
+    `origin`,
+    `destination`,
+    `rating`,
+    `reaction_id`,
+    `is_public`,
+    `date_created`,
+    `date_last_updated`,
+    `image_url`
+  )
+VALUES
+  (
+    2,
+    1,
+    'Nate\'s Second Trip',
+    'There was not a duck',
+    1,
+    'My room',
+    'The Kitchen',
+    5,
+    4,
+    DEFAULT,
+    '2020-11-28T19:23:31.000Z',
+    '2020-11-28T19:23:31.000Z',
+    'https://i.imgur.com/iXI8q.jpeg'
+  );
+INSERT INTO
+  `db`.`trip` (
+    `id`,
+    `user_id`,
+    `title`,
+    `body`,
+    `price`,
+    `origin`,
+    `destination`,
+    `rating`,
+    `reaction_id`,
+    `is_public`,
+    `date_created`,
+    `date_last_updated`,
+    `image_url`
+  )
+VALUES
+  (
+    3,
+    2,
+    'Smith\'s Trip to Maui',
+    'I had a lot of fun going to maui, I spent a lot of time on the beach',
+    5,
+    'Texas',
+    'Maui',
+    5,
+    6,
+    DEFAULT,
+    '2020-11-28T19:23:31.000Z',
+    '2020-11-28T19:23:31.000Z',
+    'https://i.imgur.com/lF3c6aK.jpg'
+  );
+COMMIT;
+-- -----------------------------------------------------
+  -- Data for table `db`.`comment`
+  -- -----------------------------------------------------
+  START TRANSACTION;
+USE `db`;
+INSERT INTO
+  `db`.`comment` (
+    `id`,
+    `trip_id`,
+    `user_id`,
+    `parent_comment_id`,
+    `date_created`,
+    `body`
+  )
+VALUES
+  (
+    DEFAULT,
+    3,
+    1,
+    NULL,
+    '2020-11-28T19:54:37.134Z',
+    'Wow! What a cool picture'
+  );
+INSERT INTO
+  `db`.`comment` (
+    `id`,
+    `trip_id`,
+    `user_id`,
+    `parent_comment_id`,
+    `date_created`,
+    `body`
+  )
+VALUES
+  (
+    DEFAULT,
+    1,
+    2,
+    NULL,
+    '2020-11-28T19:54:37.134Z',
+    'Duck.'
+  );
+INSERT INTO
+  `db`.`comment` (
+    `id`,
+    `trip_id`,
+    `user_id`,
+    `parent_comment_id`,
+    `date_created`,
+    `body`
+  )
+VALUES
+  (
+    DEFAULT,
+    1,
+    1,
+    NULL,
+    '2020-11-28T19:55:37.134Z',
+    'Duck.'
+  );
+COMMIT;
+-- -----------------------------------------------------
+  -- Data for table `db`.`like_on_trip`
+  -- -----------------------------------------------------
+  START TRANSACTION;
+USE `db`;
+INSERT INTO
+  `db`.`like_on_trip` (`id`, `liked_by_user`, `trip_id`)
+VALUES
+  (DEFAULT, 1, 3);
+INSERT INTO
+  `db`.`like_on_trip` (`id`, `liked_by_user`, `trip_id`)
+VALUES
+  (DEFAULT, 2, 1);
+INSERT INTO
+  `db`.`like_on_trip` (`id`, `liked_by_user`, `trip_id`)
+VALUES
+  (DEFAULT, 2, 2);
+COMMIT;
+-- -----------------------------------------------------
+  -- Data for table `db`.`user_saved_trip`
+  -- -----------------------------------------------------
+  START TRANSACTION;
+USE `db`;
+INSERT INTO
+  `db`.`user_saved_trip` (`id`, `trip_id`, `saved_by_user_id`)
+VALUES
+  (DEFAULT, 3, 1);
+INSERT INTO
+  `db`.`user_saved_trip` (`id`, `trip_id`, `saved_by_user_id`)
+VALUES
+  (DEFAULT, 1, 2);
+COMMIT;
