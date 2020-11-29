6b7945736e2be608d05b7a4c8af12f84fdbca306
Nathan Gage <ngage@smu.edu>
2020-11-28 21:36:36 -0600

    Create Axios stubs for user functions

--- frontend/src/Api/VacationGramAPIClient.js
+++ frontend/src/Api/VacationGramAPIClient.js
@@ -7,46 +7,72 @@ export class VacationGramAPIClient
 
   config = {};
 
-  // login(email, password)
-  // {
-  //   return new Promise((resolve, reject) =>
-  //   {
-  //     axios.get(`${this.url}/login`, { "email": email, "password": password })
-  //       .then(resp => resolve(resp.data))
-  //       .catch(resp => alert(resp));
-  //   });
-  // }
-
   login(email, password)
   {
     return new Promise((resolve, reject) =>
     {
       axios.post(
-          "http://localhost:8000/api/login",
+          `${this.url}/login`,
           {
             "email": email,
             "password": password,
-          }
+          },
         )
         .then(response => resolve(response.data))
         .catch(error => alert(error));
     });
   }
 
-  addUser(name, email, password)
+  register(name, email, password)
   {
     return new Promise((resolve, reject) =>
     {
-      axios.post(`${this.url}/addUser`, { name: name, email: email, password: password }, this.config)
-        .then(x =>
-        {
-          resolve(x.data);
-        })
-        .catch(x =>
-        {
-          alert(x);
-          reject(x);
-        });
+      axios.post(
+          `${this.url}/users`,
+          {
+            "name": name,
+            "email": email,
+            "password": password,
+          },
+        )
+        .then(response => resolve(response.data))
+        .catch(error => alert(error));
+    });
+  }
+
+  getUserInfo(id)
+  {
+    return new Promise((resolve, reject) =>
+    {
+      axios.get(
+          `${this.url}/users/${id}`,
+        )
+        .then(response => resolve(response.data))
+        .catch(error => alert(error));
+    });
+  }
+
+  getAllUsers()
+  {
+    return new Promise((resolve, reject) =>
+    {
+      axios.get(
+          `${this.url}/users`,
+        )
+        .then(response => resolve(response.data))
+        .catch(error => alert(error));
+    });
+  }
+
+  deleteUser(id)
+  {
+    return new Promise((resolve, reject) =>
+    {
+      axios.delete(
+          `${this.url}/users/${id}`,
+        )
+        .then(response => resolve(response.data))
+        .catch(error => alert(error));
     });
   }
 }
