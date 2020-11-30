631af73e66425c33ab0b1cf79f95877e5f3a5b46
petermking18 <petermking18@gmail.com>
2020-11-30 10:34:13 -0500

    Saved page delete comment works

--- frontend/src/Components/Saved.jsx
+++ frontend/src/Components/Saved.jsx
@@ -182,7 +182,11 @@ export default class Saved extends React.Component {
         console.log("Should open other profile now: " + user_id);
         this.setState({ otherProfileId: user_id, viewOtherProfile: true });
     }
-    onCommentDeletion = (postid, comments) => {
+    async deleteComment(trip_id, comment_id) {
+        await this.apiClient.deleteComment(trip_id, comment_id);
+    }
+    onCommentDeletion = (postid, comments, commentid) => {
+        this.deleteComment(postid, commentid);
         var postsArr = this.state.posts;
         for (let p = 0; p < postsArr.length; p++) {//find the post
             if (postsArr[p].id === postid) {
@@ -401,7 +405,7 @@ export default class Saved extends React.Component {
                             </button>
                         </div>
                     </div>
-                    <CommentList comments={this.state.modalPost.comments} curr_user_id={this.state.user_id} poster_id={this.state.modalPost.user_id} handleDeletion={this.onCommentDeletion} />
+                    <CommentList comments={this.state.modalPost.comments} curr_user_id={this.state.user_id} poster_id={this.state.modalPost.user_id} post_id={this.state.modalPost.post_id} handleDeletion={this.onCommentDeletion} />
                     <form className="row mt-0 ml-0 pl-0" name="newCommentForm">
                         <div className="ml-0 pl-0" id="newcommenttextarea">
                             <textarea name="newCommentTA" type="text" className="form-control mb-3" placeholder="add a comment" id="newcomment"
