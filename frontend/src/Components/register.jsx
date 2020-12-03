0494351c04569d4ddb54230130d2b81ddb1c9c4a
petermking18 <petermking18@gmail.com>
2020-12-02 20:46:06 -0500

    updated register page

--- frontend/src/Components/register.jsx
+++ frontend/src/Components/register.jsx
@@ -49,30 +49,30 @@ export class RegisterPage extends React.Component {
             this.setState({ registered: false });
         }
     } */
-    readyToRegister(){
-        if(this.state.username !== ''
-        && this.state.email !== ''
-        && this.state.password !== ''
-        && this.state.confirmPassword === this.state.password) return true;
+    readyToRegister() {
+        if (this.state.username !== ''
+            && this.state.email !== ''
+            && this.state.password !== ''
+            && this.state.confirmPassword === this.state.password) return true;
         return false;
     }
 
-    registerUser(username, email, password, confirmPassword, profilePicUrl){
-        if(profilePicUrl === '') profilePicUrl = "https://st.depositphotos.com/1779253/5140/v/600/depositphotos_51405259-stock-illustration-male-avatar-profile-picture-use.jpg";
-        if(this.readyToRegister()){
-            this.setState({confirm: true});
+    registerUser(username, email, password, confirmPassword, profilePicUrl) {
+        if (profilePicUrl === '') profilePicUrl = "https://st.depositphotos.com/1779253/5140/v/600/depositphotos_51405259-stock-illustration-male-avatar-profile-picture-use.jpg";
+        if (this.readyToRegister()) {
+            this.setState({ confirm: true });
             this.apiClient.register(username, email, password, profilePicUrl)
                 .then(user => {
                     console.log(user.info[0].id);
-                    this.setState({id: user.info[0].id});
-                    this.setState({registered: true});
+                    this.setState({ id: user.info[0].id });
+                    this.setState({ registered: true });
                 });
         }
     }
 
-    passwordsDoNotMatch(){
-        if(this.state.confirmPassword !== ''){
-            if(this.state.confirmPassword !== this.state.password){
+    passwordsDoNotMatch() {
+        if (this.state.confirmPassword !== '') {
+            if (this.state.confirmPassword !== this.state.password) {
                 return true;
             }
         }
@@ -81,76 +81,87 @@ export class RegisterPage extends React.Component {
 
     render() {
         return <>
-            <form className="container">
-                <div className="imgcontainer">
-                    <h1>Register</h1>
-                    <img src={logo} alt="Avatar" className="avatar"></img>
-                </div>
-                {this.state.registered == false && <RegisterErrorMessage />}
-                {this.state.register2 == false && <RegisterErrorMessage2 />}
-                <div className="login-form">
-                    <div className="form-group">
-                        <label htmlFor="search_name" className="py-0">Username</label>
-                        <span className="text-danger"> *</span>
-                        <input type="text"
-                            name="Email"
-                            className="form-control"
-                            value={this.state.username}
-                            onChange={e => this.setState({ username: e.target.value })} />
-                    </div>
-                </div>
+            <div id="loginPageBG">
+                <div className="fadeInDown" id="loginWrapper">
+                    <div id="loginFormContent">
+                        <form className="container">
+                            <div className="imgcontainer pt-2">
+                                <h1>Register</h1>
+                                <img src={logo} alt="Avatar" className="avatar"></img>
+                            </div>
+                            {this.state.registered == false && <RegisterErrorMessage />}
+                            {this.state.register2 == false && <RegisterErrorMessage2 />}
+                            <div className="login-form">
+                                <div className="form-group">
+                                    {/* <label htmlFor="search_name" className="py-0">Username</label> */}
+                                    {/* <span className="text-danger"> *</span> */}
+                                    <input type="text"
+                                        name="Email"
+                                        className="form-control"
+                                        placeholder="username"
+                                        value={this.state.username}
+                                        onChange={e => this.setState({ username: e.target.value })} />
+                                </div>
+                            </div>
 
-                <div className="login-form">
-                    <div className="form-group">
-                        <label htmlFor="search_name" className="py-0">Email</label>
-                        <span className="text-danger"> *</span>
-                        <input type="text"
-                            name="Email"
-                            className="form-control"
-                            value={this.state.email}
-                            onChange={e => this.setState({ email: e.target.value })} />
-                    </div>
-                </div>
-                <div className="login-form">
-                    <div className="form-group">
-                        <label htmlFor="search_name" className="py-0">Password</label>
-                        <span className="text-danger"> *</span>
-                        <input type="password"
-                            name="Password"
-                            className="form-control"
-                            value={this.state.password}
-                            onChange={e => this.setState({ password: e.target.value })} />
-                    </div>
-                </div>
+                            <div className="login-form">
+                                <div className="form-group">
+                                    {/* <label htmlFor="search_name" className="py-0">Email</label> */}
+                                    {/* <span className="text-danger"> *</span> */}
+                                    <input type="text"
+                                        name="Email"
+                                        className="form-control"
+                                        placeholder="email"
+                                        value={this.state.email}
+                                        onChange={e => this.setState({ email: e.target.value })} />
+                                </div>
+                            </div>
+                            <div className="login-form">
+                                <div className="form-group">
+                                    {/* <label htmlFor="search_name" className="py-0">Password</label> */}
+                                    {/* <span className="text-danger"> *</span> */}
+                                    <input type="password"
+                                        name="Password"
+                                        className="form-control"
+                                        placeholder="password"
+                                        value={this.state.password}
+                                        onChange={e => this.setState({ password: e.target.value })} />
+                                </div>
+                            </div>
 
-                <div className="login-form">
-                    <div className="form-group">
-                        <label htmlFor="search_name" className="py-0">Confirm Password</label>
-                        <span className="text-danger"> *</span>
-                        {this.passwordsDoNotMatch() && <p className="text-danger">Passwords do not match</p>}
-                        <input type="password"
-                            name="Password"
-                            className="form-control"
-                            value={this.state.confirmPassword}
-                            onChange={e => this.setState({ confirmPassword: e.target.value })} />
+                            <div className="login-form">
+                                <div className="form-group">
+                                    {/* <label htmlFor="search_name" className="py-0">Confirm Password</label> */}
+                                    {/* <span className="text-danger"> *</span> */}
+                                    {this.passwordsDoNotMatch() && <p className="text-danger">Passwords do not match</p>}
+                                    <input type="password"
+                                        name="Password"
+                                        className="form-control"
+                                        placeholder="confirm password"
+                                        value={this.state.confirmPassword}
+                                        onChange={e => this.setState({ confirmPassword: e.target.value })} />
+                                </div>
+                            </div>
+
+                            <div className="login-form">
+                                <div className="form-group">
+                                    {/* <label htmlFor="profPicUrl" className="py-0">Profile Picture URL</label> */}
+                                    <input type="text"
+                                        className="form-control"
+                                        placeholder="profile picture url (optional)"
+                                        value={this.state.profilePicUrl}
+                                        onChange={e => this.setState({ profilePicUrl: e.target.value })} />
+                                </div>
+                            </div>
+
+                            <div className="login-form pb-4">
+                                <button className="btn btn-primary btn-lg btn-block" type="button" onClick={() => this.registerUser(this.state.username, this.state.email, this.state.password, this.state.confirmPassword, this.state.profilePicUrl)}>Register</button>
+                                {this.state.registered && <Redirect to={'/profile/' + this.state.id} />}
+                            </div>
+                        </form>
                     </div>
                 </div>
-
-                <div className="login-form">
-                    <div className="form-group">
-                        <label htmlFor="profPicUrl" className="py-0">Profile Picture URL</label>
-                        <input type="text"
-                        className="form-control"
-                        value={this.state.profilePicUrl}
-                        onChange={e => this.setState({profilePicUrl: e.target.value})}/>
-                    </div>
-                </div>
-
-                <div className="login-form">
-                    <button className="btn btn-primary btn-lg btn-block" type="button" onClick={() => this.registerUser(this.state.username, this.state.email, this.state.password, this.state.confirmPassword, this.state.profilePicUrl)}>Register</button>
-                    {this.state.registered && <Redirect to={'/profile/' + this.state.id} />}
-                </div>
-            </form>
+            </div>
         </>;
     }
 }
