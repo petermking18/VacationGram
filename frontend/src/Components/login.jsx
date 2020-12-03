3cc0a70a0a22007661de5a529b4e19a877649feb
petermking18 <petermking18@gmail.com>
2020-12-02 20:17:05 -0500

    login effects

--- frontend/src/Components/login.jsx
+++ frontend/src/Components/login.jsx
@@ -38,40 +38,49 @@ class Login extends React.Component {
     render() {
         return <>
             <script async src="https://www.googletagmanager.com/gtag/js?id=UA-137604632-1"></script>
-            <div className="imgcontainer">
-                <h1>Welcome to TravelGram!</h1>
-                <img src={logo} alt="Avatar" className="avatar"></img>
-            </div>
 
             {this.state.authenticated === false && <ErrorMessage />}
-
-            <div className="login-form">
-                <div className="form-group">
-                    <label htmlFor="search_name">Email</label>
-                    <input type="text"
-                        name="Email"
-                        className="form-control"
-                        value={this.state.email}
-                        onChange={e => this.setState({ email: e.target.value })} />
+            <div id="loginPageBG">
+            <div className="fadeInDown" id="loginWrapper">
+                <div id="loginFormContent">
+                    <div className="imgcontainer pt-3 mb-0">
+                        <h1>Welcome to TravelGram!</h1>
+                        <img src={logo} alt="Avatar" className="avatar"></img>
+                    </div>
+                    <div className="login-form">
+                        <div className="form-group">
+                            {/* <label htmlFor="search_name">Email</label> */}
+                            <input type="text"
+                                name="Email"
+                                className="form-control"
+                                placeholder="email"
+                                value={this.state.email}
+                                onChange={e => this.setState({ email: e.target.value })} />
+                        </div>
+                    </div>
+                    <div className="login-form">
+                        <div className="form-group">
+                            {/* <label htmlFor="search_name">Password</label> */}
+                            <input type="password"
+                                name="Password"
+                                className="form-control"
+                                placeholder="password"
+                                value={this.state.password}
+                                onChange={e => this.setState({ password: e.target.value })}
+                            />
+                        </div>
+                        <button className="btn btn-primary btn-lg btn-block" type="button"
+                            onClick={() => this.onLogin()}>Log In</button>
+                        {this.state.authenticated && <Redirect to={'/home/' + this.state.id} />}
+                        <br></br>
+                        <div className="pb-4">
+                        <Link to={'register'}>
+                            <button className="btn btn-primary btn-lg mb-7 btn-block" type="button">Register</button>
+                        </Link>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="login-form">
-                <div className="form-group">
-                    <label htmlFor="search_name">Password</label>
-                    <input type="password"
-                        name="Password"
-                        className="form-control"
-                        value={this.state.password}
-                        onChange={e => this.setState({ password: e.target.value })}
-                    />
-                </div>
-                <button className="btn btn-primary btn-lg btn-block" type="button"
-                    onClick={() => this.onLogin()}>Log In</button>
-                {this.state.authenticated && <Redirect to={'/home/' + this.state.id} />}
-                <br></br>
-                <Link to={'register'}>
-                    <button className="btn btn-primary btn-lg btn-block" type="button">Register</button>
-                </Link>
             </div>
             {/* <Link to={'/home/' + this.state.id}>Skip to Home</Link> */}
         </>;
